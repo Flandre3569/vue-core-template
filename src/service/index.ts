c4fbf73857996e5365f62a8a92667113f320f627
Flandre3569 <1332726596@qq.com>
2022-02-05 00:13:47 +0800

    'env'

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,4 +1,14 @@
 import axios from "axios";
 
-const BASE_URL = "/api";
+let BASE_URL = "/api";
 const TIME_OUT = 10000;
+// 环境
+const REPORTER_HOST = import.meta.env.NODE_ENV;
+
+if (REPORTER_HOST === "development") {
+  BASE_URL = "/api";
+} else if (REPORTER_HOST === "production") {
+  BASE_URL = "/api";
+} else {
+  BASE_URL = "/api";
+}
